5e3ebda23b8d27bec4b7f5a58db48ad854d95952
Vasilik Vasiliy <vva@compent.net>
2017-06-18 17:24:34 +0300

    simple server

--- server.js
+++ server.js
@@ -1,41 +1,55 @@
-var http = require('http');
-var Static = require('node-static');
-var WebSocketServer = new require('ws');
+var express = require('express');
+var app = express();
 
-// clients
-var clients = {};
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-// WebSocket-server 8081
-var webSocketServer = new WebSocketServer.Server({port: 8081});
-webSocketServer.on('connection', function(ws, req) {
-    console.log(req)
-  var id = Math.random();
-  clients[id] = ws;
-  console.log("new connetion " + id);
-
-  ws.on('message', function(message) {
-    console.log('get message ' + message);
-
-    for(var key in clients) {
-      clients[key].send(message);
-    }
-  });
-
-  ws.on('close', function() {
-    console.log('connection closed ' + id);
-    delete clients[id];
-  });
-
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 
-// simple server at 8080
-var fileServer = new Static.Server('.');
-http.createServer(function (req, res) {
+
+
+// var http = require('http');
+// var Static = require('node-static');
+// var WebSocketServer = new require('ws');
+
+// // clients
+// var clients = {};
+
+// // WebSocket-server 8081
+// var webSocketServer = new WebSocketServer.Server({port: 8081});
+// webSocketServer.on('connection', function(ws, req) {
+//     console.log(req)
+//   var id = Math.random();
+//   clients[id] = ws;
+//   console.log("new connetion " + id);
+
+//   ws.on('message', function(message) {
+//     console.log('get message ' + message);
+
+//     for(var key in clients) {
+//       clients[key].send(message);
+//     }
+//   });
+
+//   ws.on('close', function() {
+//     console.log('connection closed ' + id);
+//     delete clients[id];
+//   });
+
+// });
+
+
+// // simple server at 8080
+// var fileServer = new Static.Server('.');
+// http.createServer(function (req, res) {
   
-  fileServer.serve(req, res);
+//   fileServer.serve(req, res);
 
-}).listen(8080);
+// }).listen(8080);
 
-console.log("СServer started at 8080, 8081");
+// console.log("СServer started at 8080, 8081");
 
