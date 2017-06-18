b1182e2224b04af6ba83a7ea5576443dcb0346a6
Vasilik Vasiliy <vva@compent.net>
2017-06-18 18:37:51 +0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
