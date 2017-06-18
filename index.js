e04bfcdae24aae33cc2e7e6517ea6f6e762b6aa9
Vasilik Vasiliy <vva@compent.net>
2017-06-18 18:39:32 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
-app.use('/static', express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+// var express = require('express');
+// var app = express();
+// app.use('/static', express.static(__dirname + '/public'));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
