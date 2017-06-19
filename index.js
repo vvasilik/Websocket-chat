9b80bf309abf5e59ce3418291e7dacb8d014b046
Vasil Vasilik <vva@compent.net>
2017-06-19 17:17:41 +0300

    fix

--- index.js
+++ index.js
@@ -9,20 +9,24 @@ app.use(express.static(__dirname + "/"))
 var server = http.createServer(app)
 server.listen(port)
 
-console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+wss.on('connection', function(ws, req) {
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("new connetion " + id);
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  ws.on('message', function(message) {
+    console.log('get message ' + message);
 
-  console.log("websocket connection open")
+    for(var key in clients) {
+      clients[key].send(message);
+    }
+  });
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+  ws.on('close', function() {
+    console.log('connection closed ' + id);
+    delete clients[id];
+  });
+
+});
