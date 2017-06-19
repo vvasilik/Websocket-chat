40ebd58659a57499fad79e393f33aefe57d937df
Vasil Vasilik <vva@compent.net>
2017-06-19 17:52:34 +0300

    fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ wss.on("connection", function(ws) {
     var id = Math.random();
     clients[id] = ws;
     for(var key in clients) {
-        clients[key].send("hi");
+        clients[key].send(clients.length);
     }
     console.log("websocket connection open")
 
