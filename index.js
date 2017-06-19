0692963e0cfb6c7127d4e3270c26045b744d3438
Vasil Vasilik <vva@compent.net>
2017-06-19 17:03:02 +0300

    fix

---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.render('public/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+var WebSocketServer = require('ws');
+
+// clients
+var clients = {};
+
+// WebSocket-server 8081
+var webSocketServer = new WebSocketServer.Server({port: 8081});
+webSocketServer.on('connection', function(ws, req) {
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("new connetion " + id);
+
+  ws.on('message', function(message) {
+    console.log('get message ' + message);
+
+    for(var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('connection closed ' + id);
+    delete clients[id];
+  });
+
+});
