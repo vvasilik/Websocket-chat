232a12e83d8ebc92427fe32007c2a4b1d3ff0fc9
Vasil Vasilik <vva@compent.net>
2017-06-19 11:45:23 +0300

    fix

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
