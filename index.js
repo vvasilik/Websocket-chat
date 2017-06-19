d8fbe0e7f2dc90121ba87744c524f6efdc714cae
Vasil Vasilik <vva@compent.net>
2017-06-19 11:47:22 +0300

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('public/index');
 });
 
 app.listen(app.get('port'), function() {
