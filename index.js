0ef117344be0864c1ff1a38f948e8f4312248c5b
Vasil Vasilik <vva@compent.net>
2017-06-19 11:53:49 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,6 +9,6 @@ app.get('/', function(request, response) {
   response.render('public/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+  // console.log('Node app is running on port', app.get('port'));
+// });
