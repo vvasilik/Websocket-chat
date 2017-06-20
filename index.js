40ade20a902abbcbb6985295fd1f583b56fd7da7
Vasil Vasilik <vva@compent.net>
2017-06-20 13:04:00 +0300

    add scroll top

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-var host = location.origin.replace(/^http/, 'ws')
-    var ws = new WebSocket(host);
-    ws.onmessage = function (event) {
-        var li = document.createElement('li');
-        li.className = "chat__frame";
-        li.innerHTML = event.data;
-        document.querySelector('.js-chat__output').appendChild(li);
-    };
+var form = document.querySelector(".js-chat__form");
+var output = document.querySelector(".js-chat__output");
+var input = form.querySelector(".js-chat__input");
+var host = location.origin.replace(/^http/, 'ws');
+var ws = new WebSocket(host);
+ws.onmessage = function (event) {
+    var li = document.createElement('li');
+    li.className = "chat__frame";
+    li.innerHTML = event.data;
+    document.querySelector('.js-chat__list').appendChild(li);
+    output.scrollTop = output.scrollHeight;
+};
 
-    var form = document.querySelector(".js-chat__form");
-    var input = form.querySelector(".js-chat__input");
-
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        ws.send(input.value);
-    })
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    ws.send(input.value);
+})
