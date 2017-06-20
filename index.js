a4da988f4cda89b4dd0c31cb91f81ac589dd1543
Vasil Vasilik <vva@compent.net>
2017-06-20 18:04:24 +0300

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
-var form = document.querySelector(".js-chat__form");
-var output = document.querySelector(".js-chat__output");
-var input = form.querySelector(".js-chat__input");
-var name = form.querySelector(".js-chat__name");
-var host = location.origin.replace(/^http/, 'ws');
-var ws = new WebSocket(host);
+function initChat() {
+    var form = document.querySelector(".js-chat__form");
+    var output = document.querySelector(".js-chat__output");
+    var input = form.querySelector(".js-chat__input");
+    var name = form.querySelector(".js-chat__name");
+    var host = location.origin.replace(/^http/, 'ws');
+    var ws = new WebSocket(host);
 
-ws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
+    ws.onmessage = function (event) {
+        var data = JSON.parse(event.data);
 
-    var li = document.createElement('li');
-    li.className = "chat__frame";
+        var li = document.createElement('li');
+        li.className = "chat__frame";
 
-    var nameEl = document.createElement('span');
-    nameEl.className = "chat__name";
-    nameEl.innerText = data.name;
+        var nameEl = document.createElement('span');
+        nameEl.className = "chat__name";
+        nameEl.innerText = data.name;
 
-    var messageEl = document.createElement('span');
-    messageEl.className = "chat__message";
-    messageEl.innerText = data.message;
+        var messageEl = document.createElement('span');
+        messageEl.className = "chat__message";
+        messageEl.innerText = data.message;
 
-    li.appendChild(nameEl);
-    li.appendChild(messageEl);
+        li.appendChild(nameEl);
+        li.appendChild(messageEl);
 
-    document.querySelector('.js-chat__list').appendChild(li);
-    output.scrollTop = output.scrollHeight;
-};
+        document.querySelector('.js-chat__list').appendChild(li);
+        output.scrollTop = output.scrollHeight;
+    };
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    var data = {
-        message: input.value,
-        name: name || "noname"
-    }
-    ws.send(JSON.stringify(data));
-    input.value = "";
-})
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        var data = {
+            message: input.value,
+            name: name || "noname"
+        }
+        ws.send(JSON.stringify(data));
+        input.value = "";
+    })
 
-var name = prompt("Enter your name : ", "noname");
+    var name = prompt("Enter your name : ", "noname");
+}
+
+initChat();
