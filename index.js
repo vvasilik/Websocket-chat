844a0ad4bd08241ced67c256a813eaf554b51421
Vasil Vasilik <vva@compent.net>
2017-06-20 13:19:58 +0300

    clear input

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var output = document.querySelector(".js-chat__output");
 var input = form.querySelector(".js-chat__input");
 var host = location.origin.replace(/^http/, 'ws');
 var ws = new WebSocket(host);
+
 ws.onmessage = function (event) {
     var li = document.createElement('li');
     li.className = "chat__frame";
@@ -14,4 +15,5 @@ ws.onmessage = function (event) {
 form.addEventListener("submit", function(e) {
     e.preventDefault();
     ws.send(input.value);
+    input.value = "";
 })
