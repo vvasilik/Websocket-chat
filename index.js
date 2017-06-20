384e933c067116b5ac876c59403ca2f6344ffeaf
Vasil Vasilik <vva@compent.net>
2017-06-20 11:10:13 +0300

    refactor

---
+++ index.js
@@ -0,0 +1,15 @@
+var host = location.origin.replace(/^http/, 'ws')
+    var ws = new WebSocket(host);
+    ws.onmessage = function (event) {
+        var li = document.createElement('li');
+        li.innerHTML = event.data;
+        document.querySelector('.js-chat__output').appendChild(li);
+    };
+
+    var form = document.querySelector(".js-chat__form");
+    var input = form.querySelector(".js-chat__input");
+
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
+        ws.send(input.value);
+    })
