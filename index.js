20da2cc4343c750b66d6c13d0dbd8a01181d1fb2
Vasilik Vasiliy <vva@compent.net>
2017-07-04 23:48:17 +0300

    add voice recording

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function initChat() {
+    recordingClass = "_recording";
     var form = document.querySelector(".js-chat__form");
     var output = document.querySelector(".js-chat__output");
     var input = form.querySelector(".js-chat__input");
+    var rec = form.querySelector(".js-chat__rec");
     var host = location.origin.replace(/^http/, 'ws');
     var ws = new WebSocket(host);
     var name;
@@ -10,7 +12,34 @@ function initChat() {
     getUserInfo();
 
     function addListeners() {
+        rec.addEventListener("click", function() {
+            setRecActive();
 
+            window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            var recognition = new SpeechRecognition();
+
+            recognition.interimResults = true;
+            recognition.addEventListener("result", function(e) {
+                var transcript = [].slice.call(e.results)
+                    .map(function(result){
+                        return result[0];
+                    })
+                    .map(function(result){
+                        return result.transcript;
+                    })
+                    .join("");
+
+                if (e.results[0].isFinal) {
+                    input.value = input.value + transcript + ". ";
+                }
+            })
+
+            recognition.addEventListener("end", function() {
+                setRecInactive();
+            });
+
+            recognition.start();
+        })
         form.addEventListener("submit", submitMessage);
 
         input.addEventListener("keydown", function(e) {
@@ -38,6 +67,16 @@ function initChat() {
             document.querySelector('.js-chat__list').appendChild(li);
             output.scrollTop = output.scrollHeight;
         };
+    }
+
+    function setRecInactive() {
+        rec.disabled = false;
+        rec.classList.remove(recordingClass);
+    }
+
+    function setRecActive() {
+        rec.disabled = true;
+        rec.classList.add(recordingClass);
     }
 
     function submitMessage(e) {
