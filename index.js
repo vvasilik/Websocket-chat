d65df6b28a444fa42db4a7da1040d2ff18259632
Vasilik Vasiliy <vva@compent.net>
2017-07-05 20:59:35 +0300

    refactor

--- index.js
+++ index.js
@@ -1,81 +1,94 @@
 function initChat() {
-    recordingClass = "_recording";
+    var recordingClass = "_recording";
+    var isRecording = false;
     var form = document.querySelector(".js-chat__form");
     var output = document.querySelector(".js-chat__output");
     var input = form.querySelector(".js-chat__input");
     var rec = form.querySelector(".js-chat__rec");
     var host = location.origin.replace(/^http/, 'ws');
     var ws = new WebSocket(host);
-    var name;
+    var recognition = createSpeechRecognition();
+    var info = getUserInfo();
+    addListeners(recognition);
 
-    addListeners();
-    getUserInfo();
+    function createSpeechRecognition() {
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        var recognitionEl = new SpeechRecognition();
+        recognitionEl.interimResults = true;
 
-    function addListeners() {
-        rec.addEventListener("click", function() {
-            setRecActive();
+        return recognitionEl;
+    }
 
-            window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-            var recognition = new SpeechRecognition();
+    function addListeners(recognitionEl) {
+        rec.addEventListener("click", recButtonAction)
 
-            recognition.interimResults = true;
-            recognition.addEventListener("result", function(e) {
-                var transcript = [].slice.call(e.results)
-                    .map(function(result){
-                        return result[0];
-                    })
-                    .map(function(result){
-                        return result.transcript;
-                    })
-                    .join("");
+        recognitionEl.addEventListener("result", getRecognitionResult);
 
-                if (e.results[0].isFinal) {
-                    input.value = input.value + transcript + ". ";
-                }
-            })
+        recognitionEl.addEventListener("end", setRecInactive);
 
-            recognition.addEventListener("end", function() {
-                setRecInactive();
-            });
-
-            recognition.start();
-        })
         form.addEventListener("submit", submitMessage);
 
         input.addEventListener("keydown", function(e) {
             if (e.keyCode === 13) submitMessage(e);
         });
 
-        ws.onmessage = function (event) {
-            var data = JSON.parse(event.data);
-            var selfClassName = "_self";
-
-            var li = document.createElement('li');
-            li.className = "chat__frame " + (name === data.name ? selfClassName : "");
-
-            var nameEl = document.createElement('span');
-            nameEl.className = "chat__name";
-            nameEl.innerText = data.name;
-
-            var messageEl = document.createElement('span');
-            messageEl.className = "chat__message";
-            messageEl.innerText = data.message;
-
-            li.appendChild(nameEl);
-            li.appendChild(messageEl);
-
-            document.querySelector('.js-chat__list').appendChild(li);
-            output.scrollTop = output.scrollHeight;
-        };
+        ws.addEventListener("message", renderMessage);
     }
 
+    function recButtonAction() {
+        if (isRecording) {
+            setRecInactive();
+            recognitionEl.stop();
+        } else {
+            setRecActive();            
+            recognitionEl.start();
+        }
+
+        isRecording = !isRecording;
+    }
+
+    function getRecognitionResult(e) {
+        var transcript = [].slice.call(e.results)
+            .map(function(result){
+                return result[0];
+            })
+            .map(function(result){
+                return result.transcript;
+            })
+            .join("");
+
+        if (e.results[0].isFinal) {
+            input.value = input.value + transcript + ". ";
+        }   
+    }
+
+    function renderMessage(e) {
+        var data = JSON.parse(e.data);
+        var selfClassName = "_self";
+
+        var li = document.createElement('li');
+        li.className = "chat__frame " + (info.name === data.name ? selfClassName : "");
+
+        var nameEl = document.createElement('span');
+        nameEl.className = "chat__name";
+        nameEl.innerText = data.name;
+
+        var messageEl = document.createElement('span');
+        messageEl.className = "chat__message";
+        messageEl.innerText = data.message;
+
+        li.appendChild(nameEl);
+        li.appendChild(messageEl);
+
+        document.querySelector('.js-chat__list').appendChild(li);
+        output.scrollTop = output.scrollHeight;
+    }
+    
     function setRecInactive() {
-        rec.disabled = false;
         rec.classList.remove(recordingClass);
-    }
+    };
 
     function setRecActive() {
-        rec.disabled = true;
         rec.classList.add(recordingClass);
     }
 
@@ -83,14 +96,18 @@ function initChat() {
         e.preventDefault();
         var data = {
             message: input.value,
-            name: name || "noname"
+            name: info.name || "noname"
         }
         ws.send(JSON.stringify(data));
         input.value = "";
     }
 
     function getUserInfo() {
-        name = prompt("Enter your name : ", "noname");
+        var name = prompt("Enter your name : ", "noname");
+
+        return {
+            name: name
+        }
     }
 }
 
